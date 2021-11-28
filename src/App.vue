d80dc3fc1fbd96927c6ac0392a320ebeb147f910
unknown <416212316@qq.com>
2021-11-28 22:04:50 +0800

    input

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <From :model="model">
-    <FormItem label="用户名" prop="username">formItem</FormItem>
+    <FormItem label="用户名" prop="username">
+        <Input v-model="model.username"></Input>
+    </FormItem>
   </From>
+  <p>{{ model }}</p>
 </template>
 
 <script setup lang="ts">
 import { reactive } from "@vue/reactivity";
 import From from './components/Form.vue'
 import FormItem from './components/FormItem.vue'
+import Input from './components/Input.vue'
 
 const model = reactive({
     username: 'coboy'
---
+++ src/components/Input.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <input :value="modelValue" @input="onInput" />
+    </div>
+</template>
+
+<script setup lang="ts">
+    defineProps({
+        modelValue: {
+            type: String,
+            default: ''
+        }
+    })
+
+    // 输出事件
+    const emit = defineEmits<{
+        (e: "update:model-value", value: string):void
+    }>()
+
+    // emit('update:model-value', 'coboy')
+
+    function onInput(e: Event) {
+        const inp = e.target as HTMLInputElement
+        emit("update:model-value", inp.value)
+    }
+</script>
