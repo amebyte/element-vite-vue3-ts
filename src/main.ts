ef97061fee34e81996f5ff84ba8a9b012129c944
unknown <416212316@qq.com>
2022-01-24 21:43:14 +0800

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from "vue";
 import App from "./App2.vue";
 import "./index.css";
+console.log("process", process);
 createApp(App).mount("#app");
--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,25 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
-import { viteMockServe } from 'vite-plugin-mock'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
+import { viteMockServe } from "vite-plugin-mock";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    port: 3005,
     proxy: {
-      '/api': {
-        target: 'http://www.baidu.com/',
+      "/api": {
+        target: "http://www.baidu.com/",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
   },
   resolve: {
     alias: {
-      '@': resolve('./src'),
-      comps: resolve('./src/components')
-    }
+      "@": resolve("./src"),
+      comps: resolve("./src/components"),
+    },
   },
-  plugins: [vue(), viteMockServe({})]
-})
+  plugins: [vue(), viteMockServe({})],
+});
