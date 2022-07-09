6b84bc2a312de30d80ae2818bfb2be0210778b60
unknown <416212316@qq.com>
2022-07-10 01:33:37 +0800

    fix

--- src/FunctionalComponent.tsx
+++ src/FunctionalComponent.tsx
@@ -1,9 +1,15 @@
-import { useReducer, useEffect, useLayoutEffect } from "vue-hooks-api";
+import {
+  useState,
+  useReducer,
+  useEffect,
+  useLayoutEffect,
+} from "vue-hooks-api";
 
 // Vue3 中函数式组件只能用函数式声明
 const FunctionalComponent = (props: any, context: any) => {
-  const [count1, setCount1] = useReducer((x: any) => x + 1, 0);
+  const [count1, setCount1] = useState(0);
   const [count2, setCount2] = useReducer((x: any) => x + 1, 1);
+  const [count3, setCount3] = useReducer((x: any) => x + 1, 2);
 
   useEffect(() => {
     console.log("useEffect", count2);
@@ -15,11 +21,14 @@ const FunctionalComponent = (props: any, context: any) => {
 
   return (
     <>
-      <button onClick={() => setCount1()} {...props}>
+      <button onClick={() => setCount1(2)} {...props}>
         count1:{count1}
       </button>
       <button onClick={() => setCount2()} {...props}>
         count2:{count2}
+      </button>
+      <button onClick={() => setCount3()} {...props}>
+        count3:{count3}
       </button>
     </>
   );
