56ac047b1e4e14f316b385d352c24944e8c83d1e
unknown <416212316@qq.com>
2022-07-09 23:49:00 +0800

    fix

--- src/Hooks2.ts
+++ src/Hooks2.ts
@@ -1,4 +1,4 @@
-import { getCurrentInstance, nextTick, watchEffect } from "vue";
+import { getCurrentInstance, watchEffect } from "vue";
 
 const HookLayout = /*    */ 0b010;
 const HookPassive = /*   */ 0b100;
@@ -16,8 +16,6 @@ function scheduleUpdateOnFiber(wip: any) {
 function renderHooks(wip: any) {
   currentlyRenderingFiber = wip;
   currentlyRenderingFiber.memorizedState = null;
-  currentlyRenderingFiber.updateQueueOfEffect = [];
-  currentlyRenderingFiber.updateQueueOfLayoutEffect = [];
   workInProgressHook = null;
 }
 
@@ -27,7 +25,6 @@ function updateWorkInProgressHook() {
   const current = currentlyRenderingFiber.alternate;
   let hook;
   if (current) {
-    // 组件更新，在老hook的基础上更新
     currentlyRenderingFiber.memorizedState = current.memorizedState;
     if (workInProgressHook) {
       // not head
@@ -39,7 +36,6 @@ function updateWorkInProgressHook() {
       currentHook = current.memorizedState;
     }
   } else {
-    // 初始渲染
     currentHook = null;
     hook = {
       memorizedState: null,
@@ -64,18 +60,30 @@ export function useReducer(reducer: any, initalState: any) {
   if (!currentlyRenderingFiber.alternate) {
     hook.memorizedState = initalState;
   }
-  const dispatch = (action: any) => {
-    hook.memorizedState = reducer(hook.memorizedState, action);
-    scheduleUpdateOnFiber(currentlyRenderingFiber);
-  };
+
+  const dispatch = dispatchReducerAction.bind(
+    null,
+    currentlyRenderingFiber,
+    hook,
+    reducer
+  );
 
   return [hook.memorizedState, dispatch];
+}
+
+function dispatchReducerAction(
+  fiber: any,
+  hook: any,
+  reducer: any,
+  action: any
+) {
+  hook.memorizedState = reducer ? reducer(hook.memorizedState) : action;
+  scheduleUpdateOnFiber(fiber);
 }
 
 function updateEffectImp(hookFlags: any, create: any, deps: any) {
   const hook = updateWorkInProgressHook();
   if (currentHook) {
-    // 检查 deps 是否变化
     const prevEffect = currentHook.memorizedState;
     if (deps) {
       const prevDeps = prevEffect.deps;
@@ -98,7 +106,7 @@ export function useLayoutEffect(create: any, deps: any) {
   return updateEffectImp(HookLayout, create, deps);
 }
 
-export function areHookInputsEqual(nextDeps: any, prevDeps: any) {
+function areHookInputsEqual(nextDeps: any, prevDeps: any) {
   if (prevDeps === null) {
     return false;
   }
