6e23e57bdbeb7a3b251cf3f5b5c9078ec3944d3c
unknown <416212316@qq.com>
2022-07-10 00:36:13 +0800

    fix

--- src/FunctionalComponent.tsx
+++ src/FunctionalComponent.tsx
@@ -1,9 +1,8 @@
 import { h, getCurrentInstance } from "vue";
-import { useReducer, useEffect, useLayoutEffect } from "./Hooks2";
+import { useReducer, useEffect, useLayoutEffect } from "vue-hooks-api";
 
 // Vue3 中函数式组件只能用函数式声明
 const FunctionalComponent = (props: any, context: any) => {
-  console.log(context, getCurrentInstance());
   const [count1, setCount1] = useReducer((x: any) => x + 1, 0);
   const [count2, setCount2] = useReducer((x: any) => x + 1, 1);
 
