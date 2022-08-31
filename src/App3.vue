b14504476167d821d959c8b2462d234a4691e871
unknown <416212316@qq.com>
2022-08-31 22:59:08 +0800

    fix

--- src/App3.vue
+++ src/App3.vue
@@ -1,6 +1,8 @@
 <template>
   <FunctionalComponent level="攻城师"></FunctionalComponent>
   <br />
+  <span>{{ state.name }}</span
+  ><span>{{ count }}</span>
   <br />
   <FunctionalComponent2 level="攻城师"></FunctionalComponent2>
 </template>
@@ -10,6 +12,10 @@ import { reactive } from "@vue/reactivity";
 import { ref, getCurrentInstance, watchEffect, onUpdated } from "vue";
 import FunctionalComponent from "./FunctionalComponent";
 import FunctionalComponent2 from "./FunctionalComponent2";
+const state = reactive({
+  name: "cobyte",
+});
+const count = ref(1);
 </script>
 
 <style scoped lang="scss">
